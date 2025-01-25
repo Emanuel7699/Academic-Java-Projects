be37613b9b2d62e437879edaf0955536ba9fc8f4
Emanuel7699 <emanuel7699@gmail.com>
2025-01-25 14:46:52 +0200

    25/1/2025
    New project

---
+++ src/MatrixList.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class MatrixList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MatrixList
+{
+    IntNodeMat _m00;
+
+    public MatrixList()
+    {
+        _m00 = null;
+    }
+
+    public MatrixList(int[][]mat)
+    {
+
+    }
+
+
+    public int getData_i_j (int i, int j)
+    {
+        return 0;
+    }
+
+    public void setData_i_j (int data, int i, int j)
+    {
+
+    }
+
+
+    public String toString()
+    {
+        return null;
+    }
+
+    public int findMax()
+    {
+        return 0;
+    }
+
+    public int howManyX(int x)
+    {
+        return 0;
+    }
+
+}
