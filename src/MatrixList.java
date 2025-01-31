911f9d61a2ec870f49144d0c44bb3185156d00b6
Emanuel7699 <emanuel7699@gmail.com>
2025-01-31 22:04:25 +0200

    31/1/25
    Add API

--- src/MatrixList.java
+++ src/MatrixList.java
@@ -1,22 +1,30 @@
 
-/**
- * Write a description of class MatrixList here.
+/** public class MatrixList.
+ * This class represents the all MAMAN questions.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Emanuel Abraham
+ * @version 31/1/2025 (2025a)
  */
 public class MatrixList
 {
     IntNodeMat _m00;
 
+    /** The method insert null while there is no matrix;
+     *
+     */
     public MatrixList()
     {
         _m00 = null;
     }
 
+    /** The method build a Linked list by the matrix.
+     *
+     * @param mat Matrix.
+     */
+
     public MatrixList(int[][]mat) {
 
-        if (mat == null || mat.length == 0 || mat[0].length == 0) {
+        if (mat == null || mat.length == 0 || mat[0].length == 0) {// If the array is empty.
             _m00 = null;
             return;
         }
@@ -26,15 +34,15 @@ public class MatrixList
         IntNodeMat ptr = _m00;
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                if(i==0 && j==0){
+                if(i==0 && j==0){//If the index in array[0][0].
                     continue;
                 }
                 IntNodeMat node = new IntNodeMat(mat[i][j]);
-                if(i==0){
+                if(i==0){//First row.
                     ptr.setNextCol(node);
                     node.setPrevCol(ptr);
                 }
-                else if(j==0){
+                else if(j==0){//First colum.
                     prevHead = head;
                     head = node;
                     ptr= node;
@@ -42,13 +50,13 @@ public class MatrixList
                     node.setPrevRow(prevHead);
                     prevHead = prevHead.getNextCol();
                 }
-                else{
+                else{//others index.
                     prevHead.setNextRow(node);
                     node.setPrevRow(prevHead);
                     ptr.setNextCol(node);
                     node.setPrevCol(ptr);
                 }
-                if (ptr.getNextCol() !=null && prevHead.getNextCol()!=null) {
+                if (ptr.getNextCol() !=null && prevHead.getNextCol()!=null) {//Promote ptr and prevHead pointers.
                     ptr = ptr.getNextCol();
                     prevHead = prevHead.getNextCol();
                 }
@@ -56,58 +64,72 @@ public class MatrixList
         }
     }
 
-
+    /** The method get the data of the index.
+     *
+     * @param i Row index.
+     * @param j Colum index.
+     * @return The data of a specific index.
+     */
     public int getData_i_j (int i, int j)
     {
-        IntNodeMat corrent = _m00;
-        if (_m00==null || i<0 || j<0){
+        IntNodeMat current = _m00;
+        if (_m00==null || i<0 || j<0){// If the pointer is out of array bonds.
             return Integer.MIN_VALUE;
         }
         else{
             int p=0,q=0;
-            while(p!=i && corrent.getNextRow() != null){
+            while(p!=i && current.getNextRow() != null){//if the row is not equals to row that gets.
                 p=p+1;
-                corrent = corrent.getNextRow();
+                current = current.getNextRow();
             }
-            while(q!=j && corrent.getNextCol() != null){
+            while(q!=j && current.getNextCol() != null){//if the colum is not equals to colum that gets.
                 q=q+1;
-                corrent = corrent.getNextCol();
+                current = current.getNextCol();
             }
-            if ( p!=i || q !=j){
+            if ( p!=i || q !=j){//if the pointer does not appear in the array.
                 return Integer.MIN_VALUE;
             }
         }
-        return corrent.getData();
+        return current.getData();
     }
 
-
+    /** The method enter a data to specific index.
+     *
+     * @param data Some data.
+     * @param i Row index.
+     * @param j Colum index.
+     */
     public void setData_i_j (int data, int i, int j)
     {
         IntNodeMat corrent = _m00;
         int p=0,q=0;
-        while(p!=i && corrent.getNextRow() != null){
+        while(p!=i && corrent.getNextRow() != null){//if the row is not equals to row that gets.
             p=p+1;
             corrent = corrent.getNextRow();
         }
-        while(q!=j && corrent.getNextCol() != null){
+        while(q!=j && corrent.getNextCol() != null){//if the colum is not equals to colum that gets.
             q=q+1;
             corrent = corrent.getNextCol();
         }
-        if( p==i && q ==j){
+        if( p==i && q ==j){//if the pointer appears in the array.
             corrent.setData(data);
         }
     }
 
+    /** The method print the list in shape of array.
+     *
+     * @return The list in shape of array.
+     */
     public String toString()
     {
-        if (_m00 == null){
+        if (_m00 == null){//if the matrix empty.
             return "";
         }
         String result = "";
         IntNodeMat nodeX = _m00;
         IntNodeMat nodeY = _m00;
-        while (nodeY != null) {
-            while (nodeX != null) {
+        while (nodeY != null) {//if the colum pointer is valid.
+            while (nodeX != null) {//if the row pointer is valid.
                 result += nodeX.getData();
                 if (nodeX.getNextCol() != null){
                     result += "\t";
@@ -118,47 +140,65 @@ public class MatrixList
             nodeX = nodeY;
             result += "\n";
         }
-        return result;
+        return result;//print the matrix.
     }
 
+    /** The method find the max number of the list.
+     *
+     * @return The max number.
+     */
     public int findMax()
     {
-        if (_m00 == null) {
+        if (_m00 == null) {//if the matrix empty.
             return Integer.MIN_VALUE;
         }
-        return findMax(_m00, Integer.MIN_VALUE,_m00);
+        return findMax(_m00, Integer.MIN_VALUE,_m00);//return the max number in the array.
     }
 
-    private int findMax(IntNodeMat corrent, int max, IntNodeMat header) {
+    /** The method find the max number of the list.
+     *
+     * @param current Current number.
+     * @param max Max number.
+     * @param header Head of row.
+     * @return The max number.
+     */
+    private int findMax(IntNodeMat current, int max, IntNodeMat header) {
 
-        max = Math.max(corrent.getData(), max);
-        if (corrent.getNextCol() != null) {
-            corrent = corrent.getNextCol();
-            return findMax(corrent, max, header);
+        max = Math.max(current.getData(), max);
+        if (current.getNextCol() != null) {//if the next colum is not empty.
+            current = current.getNextCol();
+            return findMax(current, max, header);
         } else {
-            if (header.getNextRow() != null) {
+            if (header.getNextRow() != null) {//if the next row is not empty.
                 header = header.getNextRow();
-                corrent = header;
-                return findMax(corrent, max, header);
+                current = header;
+                return findMax(current, max, header);
             } else {
-                return max;
+                return max;//return the max number
             }
         }
     }
 
+    /** This method gets a number and checks how many times the number appears in the array.
+     *
+     * @param x The number.
+     * @return The count.
+     * Time complexity o(n+m) One While, In the worst case - n+m Iteration.
+     * Space complexity o(1) There is no more an additional memory except for the counter variable.
+     */
     public int howManyX(int x)
     {
-        if (_m00 == null) {
+        if (_m00 == null) {//if the matrix empty.
             return 0;
         }
         int counter=0;
         IntNodeMat ptr = _m00;
-        while(ptr != null){
-            if(ptr.getData() == x){
+        while(ptr != null){//if the pointer is valid.
+            if(ptr.getData() == x){//if the data pointer and the number that gets are equals.
                 counter = counter+1;
                 ptr = ptr.getNextRow();
             }
-            else if(ptr.getData() <x){
+            else if(ptr.getData() <x){//if the data pointer is smaller than the number.
                 if(ptr.getNextCol() != null){
                     ptr = ptr.getNextCol();
                 }
@@ -166,7 +206,7 @@ public class MatrixList
                     ptr = ptr.getNextRow();
                 }
             }
-            else{
+            else{//if the data pointer is bigger than the number.
                 if(ptr.getPrevCol() != null){
                     ptr = ptr.getPrevCol();
                 }
@@ -175,6 +215,6 @@ public class MatrixList
                 }
             }
         }
-        return counter;
+        return counter;//Returns the count.
     }
 }
